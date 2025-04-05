8fa4b6dfbfd1053b8f3d90ce87de12bc213889a1
Gimerbone <gimerbone@gmail.com>
2025-04-06 05:45:51 +0700

    Added new component for SVG Icons

---
+++ src/components/SvgIcons.tsx
@@ -0,0 +1,35 @@
+interface IconProps {
+  className?: string
+}
+
+export const DashboardIcon: React.FC<IconProps> = ({className}): React.ReactElement => (
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M14,10V22H4a2,2,0,0,1-2-2V10Z" stroke="#000" strokeWidth="2" />
+    <path d="M22,10V20a2,2,0,0,1-2,2H16V10Z" stroke="#000" strokeWidth="2" />
+    <path d="M22,4V8H2V4A2,2,0,0,1,4,2H20A2,2,0,0,1,22,4Z" stroke="#000" strokeWidth="2" />
+  </svg>
+);
+
+export const SalesIcon: React.FC<IconProps> = ({className}): React.ReactElement => (
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <circle cx="12" cy="12" r="10" stroke="#000" strokeWidth="2" fill="none" />
+    <path d="M12 6V18" stroke="#000" strokeWidth="2" />
+    <path d="M15 8c-1-2-6-2-6 1s5 2 5 5-5 3-5 0" stroke="#000" strokeWidth="2" fill="none" />
+  </svg>
+);
+
+export const StockIcon: React.FC<IconProps> = ({className}): React.ReactElement => (
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M3 11L12 3L21 11" stroke="black" strokeWidth="2" fill="none" />
+    <rect x="6" y="11" width="12" height="10" stroke="black" strokeWidth="2" fill="none" />
+    <rect x="10" y="15" width="4" height="6" stroke="black" strokeWidth="2" fill="none" />
+  </svg>
+);
+
+export const LogoutIcon: React.FC<IconProps> = ({className}): React.ReactElement => (
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M14 3H6C5.47 3 5 3.47 5 4V20C5 20.53 5.47 21 6 21H14" stroke="black" strokeWidth="2" fill="none" />
+    <path d="M17 16L21 12L17 8" stroke="black" strokeWidth="2" fill="none" />
+    <path d="M9 12H21" stroke="black" strokeWidth="2" fill="none" />
+  </svg>
+);
