a4cd2df02e76dc95fb6aa997857a175088c7e4e8
Gimerbone <gimerbone@gmail.com>
2025-04-06 22:25:27 +0700

    Finished Login Page for dekstop

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import styles from './styles/App.module.css'
-
 import {Dashboard} from './pages/Dashboard.tsx'
 import { SignIn } from './pages/SignIn.tsx'
 
 function App() {
   return (
-    <div className={styles.root}>
-      <SignIn/>
-      {/* <Dashboard>
-        <div></div>
-      </Dashboard> */}
-    </div>
+    <SignIn/>
+    /* <Dashboard>
+      <div></div>
+    </Dashboard> */
   )
 }
 
