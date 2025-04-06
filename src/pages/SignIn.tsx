2556ee54ac5d39fe81e61ba271ff8e781794bd01
Gimerbone <gimerbone@gmail.com>
2025-04-06 08:43:24 +0700

    Renaming Login.tsx to SignIn.tsx and developing login page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/Login.module.css'
+import styles from '../styles/SignIn.module.css'
 
 import {LogoutIcon} from '../components/SvgIcons'
 
@@ -8,21 +8,36 @@ export const SignIn: React.FC = (): React.ReactElement => {
   return (
     <div className={styles.background}>
       <div>
-        <div></div>
+        <header>
+          <div className={styles.logo}>
+            {/*Logo Icon here*/}
+            <p>Mochi Travel</p>
+          </div>
+
+          <div className={styles.headerButtons}>
+            <button>Sign in</button>
+            <button>Sign Up</button>
+          </div>
+        </header>
+
         <div className={styles.loginBox}>
           <div className={styles.svgContainer}>
-            <SignInIcon/>
+            <SignInIcon className={styles.icon}/>
           </div>
 
           <p className={styles.title}>Sign in with email</p>
           <p className={styles.description}>Make your dream vacation come true. Start looking for destination now</p>
 
-          <div className={styles.inputContainer}></div>
-          <div className={styles.inputContainer}></div>
-          <p>Forgot password?</p>
+          <div className={styles.formSection}>
+            <form>
+              <div className={styles.inputContainer}></div>
+              <div className={styles.inputContainer}></div>
+            </form>
+            <p>Forgot password?</p>
 
-          <button>Get Started</button>
-          <p>Or sign in with</p>
+            <button>Get Started</button>
+            <p>Or sign in with</p>
+          </div>
 
           <div className={styles.thirdPartyLogin}></div>
           <div className={styles.thirdPartyLogin}></div>
