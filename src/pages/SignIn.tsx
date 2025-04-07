4d5135528e964a05afbe100b09f15ffc5e3a4716
Gimerbone <gimerbone@gmail.com>
2025-04-07 10:50:43 +0700

    Added favicon and work progress on login page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,15 @@
 import styles from '../styles/SignIn.module.css'
 import logo from '../assets/mochi.jpg'
+import { useState } from 'react'
 
 import {LoginIcon} from '../components/SvgIcons'
 
 export const SignIn: React.FC = (): React.ReactElement => {
+
+  const [ showPassword, setShowPassword ] = useState(false)
+
+  const togglePassword = (): void => setShowPassword((prev) => !prev);
+
   return (
     <div className={styles.background}>
       <div>
@@ -34,7 +40,11 @@ export const SignIn: React.FC = (): React.ReactElement => {
               <input type="text" placeholder='Email'/>
             </div>
             <div className={styles.inputContainer}>
-              <input type="password" placeholder='Password' />
+              <input type={showPassword ? "text" : "password"} placeholder='Password' />
+              <div 
+                className={styles.eyeIcon}
+                onClick={togglePassword}
+              ></div>
             </div>
             <p>Forgot password?</p>
             <button>Get Started</button>
