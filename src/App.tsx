e743dfcfe3b0a8d0ca883d4e1b0c4cd4f975fe7a
Gimerbone <gimerbone@gmail.com>
2025-04-08 11:08:22 +0700

    Cleaning up and small updates on Sign up UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <User />
-    </ThemeProvider>
+    <SignIn/>
   )
 }
 
