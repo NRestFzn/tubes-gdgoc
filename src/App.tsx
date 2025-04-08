11461927001d909f797fbeccdd5f7efeeda5927f
kaisaaru <ryf.kaisaaru@gmail.com>
2025-04-08 11:46:53 +0700

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { Dashboard } from './pages/Dashboard.tsx'
-import { SignIn } from './pages/SignIn.tsx'
-import Destination from './pages/Destination.tsx'
-import User from './pages/Users.tsx'
-import Vacation from './pages/Vacation.tsx'
-import Booking from './pages/Booking.tsx'
+import { Routes, Route } from "react-router-dom"
+import { SignIn } from "./pages/SignIn"
+import Destination from "./pages/Destination"
+import User from "./pages/Users"
+import Vacation from "./pages/Vacation"
+import Booking from "./pages/Booking"
 import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <User />
+      <Routes>
+        <Route path="/" element={<Destination />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/destinations" element={<Destination />} />
+        <Route path="/users" element={<User />} />
+        <Route path="/vacations" element={<Vacation />} />
+        <Route path="/bookings" element={<Booking />} />
+      </Routes>
     </ThemeProvider>
   )
 }
