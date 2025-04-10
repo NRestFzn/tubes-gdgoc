6288ec05af3d9b95ce12b313b25bd3b2cee4dbf6
zoovasoup <zulfa.plank@proton.me>
2025-04-10 22:19:51 +0700

    add sidebar button active state

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,24 +1,14 @@
 "use client"
 
-import { Link } from "react-router-dom"
-import { ChevronRight, type LucideIcon } from "lucide-react"
-
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible"
+import { type LucideIcon } from "lucide-react"
 import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
-
+import { Link, useLocation } from "react-router"
 export function NavMain({
   items,
 }: {
@@ -33,20 +23,25 @@ export function NavMain({
     }[]
   }[]
 }) {
+  const location = useLocation()
+
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Manage</SidebarGroupLabel>
       <SidebarMenu>
-        {items.map((item) => (
-          <SidebarMenuItem key={item.title}>
-            <Link to={item.url}>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
-              </SidebarMenuButton>
-            </Link>
-          </SidebarMenuItem>
-        ))}
+        {items.map((item) => {
+          const isActive = location.pathname === item.url;
+          return (
+            <SidebarMenuItem key={item.title}>
+              <Link to={item.url}>
+                <SidebarMenuButton tooltip={item.title} isActive={isActive}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          )
+        })}
       </SidebarMenu>
     </SidebarGroup>
   )
